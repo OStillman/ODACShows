30a36cc8649f55d104152ce8a1a2f51d8d066439
Owen Stillman <owenstillman@live.co.uk>
2020-12-13 13:54:40 +0000

    Project Setup

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, flash, redirect, url_for, render_template, request
+
+from endpoints.endpoint_control import endpoint_control
+
+app = Flask(__name__)
+
+app.register_blueprint(endpoint_control, url_prefix='/ODAC/shows')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
