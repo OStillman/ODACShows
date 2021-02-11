7596745c4add15388ad627c5db2b26f744a68f61
Owen Stillman <owenstillman@live.co.uk>
2021-02-11 10:45:00 +0000

    Setup On demand endpoint method

--- endpoints/api_endpoint_control.py
+++ endpoints/api_endpoint_control.py
@@ -7,3 +7,13 @@ api_endpoint_control = Blueprint('api_endpoint_control', __name__)
 @api_endpoint_control.route('/')
 def index():
     return "ok"
+
+@api_endpoint_control.route('/OD', methods=['GET', 'POST'])
+def on_demand():
+    if request.method ==  "POST":
+        return "Post"
+    elif request.method == "GET":
+        return "Get"
+    else:
+        return "Error.."
+
