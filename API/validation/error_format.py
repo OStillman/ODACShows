a95445f8722ed544dcb874589f578eb118cf98df
Owen Stillman <owenstillman@live.co.uk>
2021-02-12 16:57:28 +0000

    Add Comments for Schema Validation

--- API/validation/error_format.py
+++ API/validation/error_format.py
@@ -5,17 +5,22 @@ class FormatValidationError():
         self.error = error
 
     def schema_validation_error(self):
-        #({'success': True}), 201, {'ContentType': 'application/json'}
+        # If the error is schema validation related, the below should help the user narrow down the issue
         try:
+            # If the schema specifies a length e.g. Array must have 1 element
             if isinstance(self.error.validator_value, int):
                 message = ("Error caused by '{}' attribute must be at least {} in length").format(self.error.path[0], self.error.validator_value)
             else:
+                # Otherwise, it's most likely a type issue
                 message = ("Error caused by '{}' attribute not being a {}").format(self.error.path[0], self.error.validator_value)
         except IndexError:
+            # If we get this far, it's as an element is missing e.g. Service is missing from OD
             message = ("Error caused as {} is {}").format(self.error.validator_value, self.error.validator)
         except:
+            # Otherwise, who knows?!
             message = ("Unknown Error, all I know for sure is {} caused it").format(self.error.validator_value)
         finally:
+            # We can then spit out our message
             return (json.dumps({
                 'success': False,
                 'message' : message
