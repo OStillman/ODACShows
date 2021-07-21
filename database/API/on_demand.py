f6e905b3342c4631b2690b4ec02ce07cc5182e2c
Owen Stillman <owenstillman@live.co.uk>
2021-07-21 16:26:22 +0100

    Add comments

--- database/API/on_demand.py
+++ database/API/on_demand.py
@@ -4,15 +4,18 @@ from database.API import tags
 
 class AddODShow():
     def __init__(self, show_info):
+        #Show info = JSON input from user
         self.show_info = show_info
         self.DBAdd = db_actions.DBAdd()
 
     def ExecuteAdd(self):
+        # To Add, we need to execute the statement, check and add the tags, then close the DB
         sql_statement = self.execute_statement()
         self.tag_control()
         self.DBAdd.close_db()
 
     def execute_statement(self):
+        # Execute Add Statement
         self.DBAdd.cursor.execute('''
         INSERT INTO ODShows (
             name,
@@ -31,9 +34,13 @@ class AddODShow():
         ''', (self.show_info["name"], self.show_info["service"], self.show_info["watching"], self.show_info["episode"], self.show_info["series"]))
 
     def tag_control(self):
+        # Deal with tag(s)
+        # First, get the last row we've just added - for the link table reference
         show_id = self.DBAdd.retrieve_last_row_id()
         for tag in self.show_info['tags']:
+            # Check tag exists, and if not create
             tag_id = self.checkTagExists(tag)
+            # Using this, we can then add our tag - show link
             self.DBAdd.cursor.execute('''
             INSERT INTO ShowTags(
                 show_id,
@@ -54,7 +61,10 @@ class AddODShow():
             WHERE name = ?;
             ''', (tag, ))
         try:
+            # If tag exists, will be able to fetch the output
             return select_cursor.fetchall()[0][0]
         except IndexError:
+            # Or will error, and so the tag doesn't exist, so we need to create it
             AddTag = tags.AddTag(self.DBAdd.cursor)
+            # Will return the tag's row_id
             return AddTag.add_tag(tag)
