576009d6ebde271cf08e4cddba8c3891febd25e1
Owen Stillman <owenstillman@live.co.uk>
2021-08-20 14:15:38 +0100

    Add API Driven Outputs

--- App/odacshows/lib/helpers/constants.dart
+++ App/odacshows/lib/helpers/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 Color color_primary = Color.fromRGBO(44,102,110,100);
 Color color_secondary = Color.fromRGBO(128,179,156,100);
+
+String base_url = "http://192.168.68.139:5000";
--- App/odacshows/lib/on_demand/on_demand_entry.dart
+++ App/odacshows/lib/on_demand/on_demand_entry.dart
@@ -14,29 +14,29 @@ class OnDemandEntry extends StatefulWidget {
 
 class _OnDemandEntry extends State<OnDemandEntry> {
   late Future<OnDemandShows> futureODShows;
-  Future<OnDemandShows> fetchODShows() async{
+
+  Future<OnDemandShows> fetchODShows() async {
     OnDemandShows onDemandShows;
-    final response = await http.get(Uri.parse('http://192.168.68.139:5000/ODAC/shows/api/od'));
-    if (response.statusCode == 200){
+    final response = await http
+        .get(Uri.parse('$base_url/ODAC/shows/api/od'));
+    if (response.statusCode == 200) {
       print("Request Succeeded");
       //print(response.body);
       var onDemandShowsJSON = json.decode(response.body);
       onDemandShows = OnDemandShows.fromJson(onDemandShowsJSON);
       return onDemandShows;
       //return OnDemandShows.fromJson(jsonDecode(response.body));
-    }
-    else{
+    } else {
       throw Exception('Failed to load On Demand Shows');
     }
   }
-
 
   void _incrementCounter() {
     setState(() {});
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     futureODShows = fetchODShows();
   }
@@ -50,26 +50,102 @@ class _OnDemandEntry extends State<OnDemandEntry> {
         ),
         body: FutureBuilder<OnDemandShows>(
           future: futureODShows,
-          builder: (context, snapshot){
+          builder: (context, snapshot) {
             if (snapshot.hasData) {
-              print(snapshot.data);
               var data = snapshot.data;
-              if (data != null){
-                print(data.odShows[0].id);
+              if (data != null) {
+                return OutputOD(data.odShows);
+                //return Text("Works");
               }
-              return Text("Works");
+              else{
+                return Text("No Shows?!");
+              }
             } else if (snapshot.hasError) {
               return Text('${snapshot.error}');
             }
 
             // By default, show a loading spinner.
-            return const CircularProgressIndicator();
+            return DefaultLoading();
           },
-        )
+        ));
+  }
+
+  Widget OutputOD(all_od_shows){
+    return GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: const EdgeInsets.all(20),
+        children: FormatODShows(all_od_shows)
     );
   }
 
-  /*@override
+  List<Widget> FormatODShows(all_od_shows){
+    List<Widget> all_widgets = [];
+    for (var show in all_od_shows){
+      Widget this_widget;
+      String progress;
+      String show_name = show.name;
+      String watching = show.watching;
+
+      if (show.episode == 0 && show.series == 0){
+        progress = "";
+      }
+      else{
+        String episode = show.episode.toString();
+        String series = show.series.toString();
+        progress = "S$series, Ep$episode";
+      }
+
+      if (watching == "Y"){
+        this_widget = Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.cyan[800],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[Text("$show_name", style: TextStyle(color: Colors.white)), Spacer(), Text(progress, style: TextStyle(fontWeight: FontWeight.w100, color: Colors.white),)],
+            ));
+      }
+      else{
+        this_widget = Opacity(
+          opacity: 0.4,
+          child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.cyan[800],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Text("Test Show", style: TextStyle(color: Colors.white)), Spacer(),],
+              )),
+        );
+      }
+
+      all_widgets.add(this_widget);
+    }
+    return all_widgets;
+  }
+
+  Widget DefaultLoading() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(
+            color: Colors.cyan,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: Text(
+              "Loading, Please Wait...",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+/*@override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[800],
