e173f3bf182f21cbb48af62b5b13ce1c5fa4d237
Owen Stillman <owenstillman@live.co.uk>
2021-11-08 16:16:48 +0000

    Add Allowance for Multi-Digit Episodes and Series

--- API/live_shows/episode_logic.py
+++ API/live_shows/episode_logic.py
@@ -24,15 +24,17 @@ class EpisodeNumber():
     def episodeBranchTwo(self, start_point = 0):
         description = self.show_details["description"]
         find_point = description.find("Ep", start_point)
-        print(find_point)
 
         if find_point != -1:
             print("Found AN Ep")
             potential_ep_number = find_point + 2
             print(description[potential_ep_number])
+            if description[potential_ep_number] == " ":
+                potential_ep_number = potential_ep_number + 1
             if description[potential_ep_number].isnumeric():
                 print("Found it!")
-                return description[potential_ep_number]
+                return self.checkMoreThanOneDigit(description, potential_ep_number)
+                #return description[potential_ep_number]
             else:
                 print(potential_ep_number+2)
                 if potential_ep_number + 2 < len(description):
@@ -41,3 +43,16 @@ class EpisodeNumber():
                     return False
         else:
             return False
+
+    def checkMoreThanOneDigit(self, description, ep_number):
+        this_ep_number = str(description[ep_number])
+        ep_number = ep_number + 1
+        is_numeric = True
+        while is_numeric:
+            this_char = description[ep_number]
+            if this_char.isnumeric():
+                this_ep_number = this_ep_number + str(this_char)
+            else:
+                is_numeric = False
+            ep_number = ep_number + 1
+        return this_ep_number
--- API/live_shows/series_logic.py
+++ API/live_shows/series_logic.py
@@ -29,9 +29,12 @@ class SeriesNumber():
             print("Found AN S")
             potential_series_number = find_point + 1
             print(description[potential_series_number])
+            if description[potential_series_number] == " ":
+                potential_series_number = potential_series_number + 1
             if description[potential_series_number].isnumeric():
                 print("Found it!")
-                return description[potential_series_number]
+                return self.checkMoreThanOneDigit(description, potential_series_number)
+                #return description[potential_series_number]
             else:
                 if potential_series_number + 1 < len(description):
                     return(self.seriesBranchTwo(potential_series_number))
@@ -39,3 +42,18 @@ class SeriesNumber():
                     return False
         else:
             return False
+
+
+    def checkMoreThanOneDigit(self, description, series_number):
+        this_series_number = str(description[series_number])
+        series_number = series_number + 1
+        is_numeric = True
+        while is_numeric:
+            this_char = description[series_number]
+            if this_char.isnumeric():
+                this_series_number = this_series_number + str(this_char)
+            else:
+                is_numeric = False
+            series_number = series_number + 1
+        return this_series_number
+
